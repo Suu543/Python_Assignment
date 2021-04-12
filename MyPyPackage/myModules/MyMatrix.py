23216fa8897cbf77aeed3e0b976a1d61028c95eb
jos50275266 <jos50275266@gmail.com>
2021-04-12 22:51:17 +0900

    modification

--- MyPyPackage/myModules/MyMatrix.py
+++ MyPyPackage/myModules/MyMatrix.py
@@ -15,7 +15,7 @@ def mtrxSub(A, B):
     return result
 
 def mtrxMul(A, B):
-    result = [len(B[1]) * [1] for i in range(len(A))]
+    result = [len(B[0]) * [0] for i in range(len(A))]
     for i in range(0, len(A)):
         for j in range(0, len(A)):
             result[i][j] = A[i][j] * B[i][j]
